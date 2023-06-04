a1e5057da5d655b93857490c09b2da4e884a7d21
byiringiroscar <byiringoroscar@gmail.com>
2023-06-04 11:55:09 +0200

    Add db setup

---
+++ database.py
@@ -0,0 +1,16 @@
+import sqlalchemy as _sql
+import sqlalchemy.ext.declarative as _declarative
+import sqlalchemy.orm as _orm
+
+# Create a declarative base class
+DATABASE_URL = "sqlite:///./database.db"
+
+# Create a declarative base class
+engine = _sql.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+Base = _declarative.declarative_base(bind=engine)
+
+# Create a session class
+SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+
---
+++ models.py
@@ -0,0 +1,4 @@
+import datetime as _dt
+import sqlalchemy as _sql
+import sqlalchemy.orm as _orm
+import passlib.hash as _hash
